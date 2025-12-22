fa83a31495cd61618a177d11ee5bd3f10634619e
Claudio Neto <craudiofelix@gmail.com>
2025-12-22 18:06:04 -0300

    .

---
+++ src/infra/http/controllers/authenticate.controller.e2e-spec.ts
@@ -0,0 +1,49 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { AppModule } from '@/app.module';
+import { hash } from 'bcryptjs';
+import request from 'supertest';
+
+describe('Authenticate (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaClient;
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    prisma = moduleRef.get(PrismaClient);
+
+    await app.init();
+  });
+
+  test('[POST] /sessions', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        cpf: '11122233344',
+        password: await hash('123456', 8),
+        roles: ['ADMIN'],
+      },
+    });
+
+    const response = await request(app.getHttpServer()).post('/sessions').send({
+      cpf: '11122233344',
+      password: '123456',
+    });
+
+    expect(response.statusCode).toBe(201);
+
+    expect(response.body).toEqual({
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      access_token: expect.any(String),
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      refresh_token: expect.any(String),
+    });
+  });
+});
--- test/factories/make-user.ts
+++ test/factories/make-user.ts
@@ -1,6 +1,9 @@
 import { faker } from '@faker-js/faker';
 import { User, UserProps } from '@/domain/enterprise/entities/user';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { hash } from 'bcryptjs';
 
 export function makeUser(
   override: Partial<UserProps> = {},
@@ -18,4 +21,24 @@ export function makeUser(
   );
 
   return user;
+}
+
+@Injectable()
+export class UserFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaUser(overrides = {}) {
+    return this.prisma.user.create({
+      data: {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        cpf: faker.number
+          .int({ min: 10000000000, max: 99999999999 })
+          .toString(),
+        password: await hash('123456', 8),
+        roles: ['DELIVERER'],
+        ...overrides,
+      },
+    });
+  }
 }
